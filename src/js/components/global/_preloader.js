56195e76b86fc9449051af9b0cef37b48123490a
SineYlo <56423781+SineYlo@users.noreply.github.com>
2022-06-24 19:26:54 +1000

    feat: preloader added

---
+++ src/js/components/global/_preloader.js
@@ -0,0 +1,23 @@
+import {
+  disablePageScroll,
+  enablePageScroll,
+  addFillGapTarget,
+} from 'scroll-lock';
+
+const siteBody = document.querySelector('.site-body');
+const preloader = document.querySelector('.preloader');
+
+disablePageScroll(preloader);
+addFillGapTarget(preloader);
+
+window.addEventListener('load', () => {
+  window.setTimeout(() => {
+    siteBody.classList.add('ui-preloader-hidden');
+    enablePageScroll(preloader);
+  }, 1500);
+
+  window.setTimeout(() => {
+    siteBody.classList.add('ui-site-loaded');
+    siteBody.classList.remove('ui-preloader-hidden');
+  }, 2000);
+});
--- src/js/home-scripts.js
+++ src/js/home-scripts.js
@@ -19,5 +19,7 @@ import './components/home/_swiper-useful-section';
 import './components/home/_tooltip-callback-section';
 // |=============== CONNECTING THE SCRIPT | JUST VALIDATE ===============>
 import './components/home/_validation-callback-form';
+// |=============== CONNECTING THE SCRIPT | PRELOADER ===============>
+import './components/global/_preloader';
 // |=============== CONNECTING THE SCRIPT | OBSERVER ===============>
 import './components/global/_observer';
